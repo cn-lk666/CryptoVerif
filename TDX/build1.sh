74411b2191d52f3974f87c5fb9ff699b9e2da6a3
Jing <jingtud@outlook.com>
2021-10-04 15:21:23 +0200

    test1.pcv is the overview of tdx

---
+++ TDX/build1.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -e
+
+file_exists_or_abort()
+{
+    filename=$1
+    if [ ! -f $filename ]
+    then
+	echo "File '$filename' not found. Did you execute this script in the directory in which it's stored?"
+	exit 2
+    fi
+}
+
+file_exists_or_abort TDX.pcv
+
+if [ -x ../../cryptoverif ]
+then
+    CV=../../cryptoverif
+else
+    CV=~/Downloads/cryptoverif2.04/cryptoverif
+fi
+
+CRYPTOKIT="-linkpkg -package cryptokit"
+
+echo Proving the protocol...
+"$CV" test1.pcv
+"$CV" -impl test1.pcv
+
